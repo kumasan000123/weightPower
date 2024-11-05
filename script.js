01cdb582dde7c98a63b10be9d1e6b8d8f8696b24
kumazawa <kumasan000123@gmail.com>
2024-11-05 11:09:51 +0900

    入力の条件をつけてレスポンシブに変更

--- script.js
+++ script.js
@@ -11,23 +11,43 @@ const deadliftresultElm=document.getElementById("deadliftresult");
 //const allresultElm=document.getElementById("allresult");
 
 showbtnElm.addEventListener("click",()=>{
-    const weight=document.getElementById("weight").value;
-    const bench=document.getElementById("bench").value;
-    const squat=document.getElementById("squat").value;
-    const deadlift=document.getElementById("deadlift").value;
+    const weight=parseFloat(document.getElementById("weight").value);
+    const bench=parseFloat(document.getElementById("bench").value);
+    const squat=parseFloat(document.getElementById("squat").value);
+    const deadlift=parseFloat(document.getElementById("deadlift").value);
+    if(weight<=0){
+        return alert("体重が入力されていないか正常な数値ではありません");
+    }
     const benchLevel=benchWeigh(weight,bench);
     const squatLevel=squatWeigh(weight,squat);
     const deadliftLevel=deadliftWeigh(weight,deadlift);
-    benchLavelElm.textContent="ベンチプレスのレベルは";
-    benchresultElm.textContent=benchLevel;
+    // 結果の初期化
+    benchLavelElm.textContent = "";
+    benchresultElm.textContent = "";
+    squatLavelElm.textContent = "";
+    squatresultElm.textContent = "";
+    deadliftLavelElm.textContent = "";
+    deadliftresultElm.textContent = "";
+
+    if(benchLevel!==0){
+        benchLavelElm.textContent="ベンチプレスのレベルは";
+        benchresultElm.textContent=benchLevel;
+    }
+    if(squatLevel!==0){
     squatLavelElm.textContent="スクワットのレベルは";
     squatresultElm.textContent=squatLevel;
-    deadliftLavelElm.textContent="デッドリフトのレベルは";
-    deadliftresultElm.textContent=deadliftLevel;
+    }
+    if(deadliftLevel!==0){
+        deadliftLavelElm.textContent="デッドリフトのレベルは";
+        deadliftresultElm.textContent=deadliftLevel;
+    }
 })
 
 //判定用の関数
 function benchWeigh(weight,bench){
+    if(bench===0 || bench<=0 || bench===null || bench===undefined ||isNaN(bench)){
+        return 0;
+    }
     const power=bench/weight;
     if(power<0.5){
         return "超初心者";
@@ -42,6 +62,9 @@ function benchWeigh(weight,bench){
     }
 }
 function squatWeigh(weight,squat){
+    if(squat===0 || squat<=0 || squat===null || squat===undefined ||isNaN(squat)){
+        return 0;
+    }
     const power=squat/weight;
     if(power<0.75){
         return "超初心者";
@@ -56,6 +79,9 @@ function squatWeigh(weight,squat){
     }
 }
 function deadliftWeigh(weight,deadlift){
+    if(deadlift===0 || deadlift<=0 || deadlift===null || deadlift===undefined ||isNaN(deadlift)){
+        return 0;
+    }
     const power=deadlift/weight;
     if(power<1){
         return "超初心者";
