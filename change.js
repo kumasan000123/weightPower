de011ee4067cbaad4e7643e69cfe358071bfd8e7
kumazawa <kumasan000123@gmail.com>
2024-11-06 09:32:26 +0900

    微調整

--- change.js
+++ change.js
@@ -6,24 +6,25 @@ showbtnElm.addEventListener("click",()=>{
     const category=document.getElementById("category").value;
     const kg=parseFloat(document.getElementById("kg").value);
     const rep=Number(document.getElementById("rep").value);
+    // 入力を確認
     if(kg<=0 || kg===null || kg===undefined ||isNaN(kg)){
-        return alert("数値入力してください");
+        return alert("重量を数値で入力してください");
     }
     if(rep<=0 || rep===null || rep===undefined || isNaN(rep)){
-        return alert("数値を入力してください");
+        return alert("回数を整数値をで入力してください");
     }
     switch(category){
         case "bench":
-            resultCategoryElm.textContent="ベンチプレスの1RMは";
+            resultCategoryElm.textContent="ベンチプレスの1RMは約";
             rmElm.textContent=(kg*rep/40+kg).toFixed(1)+"kg";
             break;
         case "squat":
-            resultCategoryElm.textContent="スクワットの1RMは";
-            rmElm.textContent=(kg*rep/33.3+kg).toFixed(1)+"kg";
+            resultCategoryElm.textContent="スクワットの1RMは約";
+            rmElm.textContent=(kg*(1+rep/33.3)).toFixed(1)+"kg";
             break;
         case "deadlift":
-            resultCategoryElm.textContent="デッドリフトの1RMは";
-            rmElm.textContent=(kg*rep/33.3+kg).toFixed(1)+"kg";
+            resultCategoryElm.textContent="デッドリフトの1RMは約";
+            rmElm.textContent=(kg*(1+rep/33.3)).toFixed(1)+"kg";
             break;
 
     }
